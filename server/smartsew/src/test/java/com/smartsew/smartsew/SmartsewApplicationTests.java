7c4088efc4135025a29cef581aca036f6b37811b
KenTheWhale <kienquoc11026789@gmail.com>
2025-04-11 15:11:51 +0700

    init: change project structure

--- server/smartsew/src/test/java/com/smartsew/smartsew/SmartsewApplicationTests.java
+++ server/smartsew/src/test/java/com/smartsew/smartsew/SmartsewApplicationTests.java
@@ -1,13 +1,13 @@
-package com.smartsew.account_service;
+package com.smartsew.smartsew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountServiceApplicationTests {
+class SmartsewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
